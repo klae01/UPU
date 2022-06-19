d2bc907e2e1d713d5df6098083e7e0e6cae0be9a
klae01 <tspt2479@gmail.com>
2022-06-19 21:02:02 +0900

    add faster method

--- layers/base/bconv2d.py
+++ layers/base/bconv2d.py
@@ -2,31 +2,44 @@ import torch
 import torch.nn as nn
 
 
-class BConv2d(nn.Conv2d):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class BConv2d_base(nn.Conv2d):
+    @classmethod
+    def reweight_mapping(
+        cls, x, weight, kernel_size, stride, padding, dilation, groups
+    ):
+        B, C, H, W = x.shape
+        return torch.conv2d(
+            torch.ones((1, C, H, W), device=x.device, dtype=x.dtype),
+            weight.square(),
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+        ).sqrt()
 
     def forward(self, x):
         if self.padding_mode != "zeros":
             return super().forward(x)
-        with torch.no_grad():
-            expected_power = nn.functional.conv2d(
-                torch.ones_like(x),
-                self.weight.square(),
-                stride=self.stride,
-                padding=self.padding,
-                dilation=self.dilation,
-                groups=self.groups,
-            ).sqrt()
+        expected_power = type(self).reweight_mapping(
+            x,
+            self.weight,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            groups=self.groups,
+        )
         x = self._conv_forward(x, self.weight, None) / (expected_power + 1e-6)
         if self.bias is not None:
             x = x.add(self.bias.view(-1, 1, 1))
         return x
 
 
-class BConv2d_fast(nn.Conv2d):
-    @staticmethod
-    def reweight_mapping(x, weight, kernel_size, stride, padding, dilation):
+class BConv2d_v1(BConv2d_base):
+    @classmethod
+    def reweight_mapping(
+        cls, x, weight, kernel_size, stride, padding, dilation, groups
+    ):
         B, C, H, W = x.shape
 
         WS = weight.square()  # [O, I / G, Kh, Kw]
@@ -45,6 +58,7 @@ class BConv2d_fast(nn.Conv2d):
             torch.ones((1, 1, H), **op_kwargs), SQ, stride=S, padding=P, dilation=D
         )
 
+        # Width processing
         SQ = WS.sum(dim=1).unsqueeze(1)  # [O, 1, Kw]
         S, P, D = stride[1], padding[1], dilation[1]
         W_cutoffs = torch.conv1d(
@@ -94,19 +108,5 @@ class BConv2d_fast(nn.Conv2d):
 
         return torch.relu(output).sqrt()
 
-    def forward(self, x):
-        if self.padding_mode != "zeros":
-            return super().forward(x)
-        with torch.no_grad():
-            expected_power = BConv2d_fast.reweight_mapping(
-                x,
-                self.weight,
-                kernel_size=self.kernel_size,
-                stride=self.stride,
-                padding=self.padding,
-                dilation=self.dilation,
-            )
-        x = self._conv_forward(x, self.weight, None) / (expected_power + 1e-6)
-        if self.bias is not None:
-            x = x.add(self.bias.view(-1, 1, 1))
-        return x
+
+BConv2d = BConv2d_v1
