f27ce3b325a0050664092f252fe6cfc7af9886a0
klae01 <tspt2479@gmail.com>
2022-06-19 17:08:54 +0900

    remove useless operations

--- layers/base/bconv2d.py
+++ layers/base/bconv2d.py
@@ -27,53 +27,53 @@ class BConv2d(nn.Conv2d):
 class BConv2d_fast(BConv2d):
     @staticmethod
     def reweight_mapping(x, weight, kernel_size, stride, padding, dilation, groups):
-        def build_cumsum(x, dim, reverse):
-            if reverse:
-                return x.filp(dim).cumsum(dim=dim).filp(dim)
-            return x.cumsum(dim=dim)
-
         B, C, H, W = x.shape
 
         WS = weight.square()  # [O, I / G, Kh, Kw]
-        WS = WS.tensor_split(groups, dim=1).sum(dim=1)  # [O, Kh, Kw]
+        WS = WS.sum(dim=1)  # [O, Kh, Kw]
 
         # Height processing
         SQ = WS.sum(dim=2).unsqueeze(1)  # [O, 1, Kh]
         S, P, D = stride[0], padding[0], dilation[0]
         H_cutoffs = torch.conv1d(
-            torch.ones((B, 1, H + P * 2)), weight=SQ, stride=S, dilation=D
+            torch.ones((1, 1, H + P * 2)), weight=SQ, stride=S, dilation=D
         ) - torch.conv1d(
-            torch.ones((B, 1, H)), weight=SQ, stride=S, padding=P, dilation=D
+            torch.ones((1, 1, H)), weight=SQ, stride=S, padding=P, dilation=D
         )
 
         SQ = WS.sum(dim=1).unsqueeze(1)  # [O, 1, Kw]
         S, P, D = stride[1], padding[1], dilation[1]
         W_cutoffs = torch.conv1d(
-            torch.ones((B, 1, W + P * 2)), weight=SQ, stride=S, dilation=D
+            torch.ones((1, 1, W + P * 2)), weight=SQ, stride=S, dilation=D
         ) - torch.conv1d(
-            torch.ones((B, 1, W)), weight=SQ, stride=S, padding=P, dilation=D
+            torch.ones((1, 1, W)), weight=SQ, stride=S, padding=P, dilation=D
         )
 
         # output = torch.empty((WS.size(0), H_cutoffs.size(2), W_cutoffs.size(2)))
-        output = WS.sum((1, 2)) - H_cutoffs.unsqueeze(2) - W_cutoffs.unsqueeze(1)
+        print(WS.shape, H_cutoffs.shape, W_cutoffs.shape)
+        output = WS.sum((1, 2), True) - H_cutoffs.unsqueeze(3) - W_cutoffs.unsqueeze(2)
 
         valid_ws = [
-            (s + P * 2) % (S * D)
-            for s, S, P, D in zip([H, W], stride, padding, dilation)
+            ((s + P * 2) % (S * D)) + K * S * D
+            for s, K, S, P, D in zip([H, W], kernel_size, stride, padding, dilation)
         ]
-        edges = torch.conv1d(
-            torch.ones((B, 1, *valid_ws)),
+        print(valid_ws)
+        print(padding)
+        edges = torch.conv2d(
+            torch.ones((1, 1, *valid_ws)),
             weight=WS.unsqueeze(1),
             stride=stride,
             padding=padding,
             dilation=dilation,
         )
+        print(edges.shape)
+        print(output.shape)
 
-        Eh, Ew = [P + K for P, K in zip(padding, kernel_size)]
-        output[:, :Eh, :Ew] = edges[:, :Eh, :Ew]
-        output[:, :Eh, -Ew:] = edges[:, :Eh, -Ew:]
-        output[:, -Eh:, :Ew] = edges[:, -Eh:, :Ew]
-        output[:, -Eh:, -Ew:] = edges[:, -Eh:, -Ew:]
+        Eh, Ew = [P for P, K in zip(padding, kernel_size)]
+        output[..., :Eh, :Ew] = edges[..., :Eh, :Ew]
+        output[..., :Eh, -Ew:] = edges[..., :Eh, -Ew:]
+        output[..., -Eh:, :Ew] = edges[..., -Eh:, :Ew]
+        output[..., -Eh:, -Ew:] = edges[..., -Eh:, -Ew:]
 
         return torch.nn.functional.relu(output, inplace=True).sqrt()
 
